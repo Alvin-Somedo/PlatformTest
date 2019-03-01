a3d7e4f8bd63487a870068ef6867ee4eefd788bc
Alvin Zhu <alvinzsx@gmail.com>
2019-03-01 11:46:14 +0800

    ttt

--- Alvin.TrackingTodo.Windows/Program.cs
+++ Alvin.TrackingTodo.Windows/Program.cs
@@ -14,6 +14,7 @@ namespace Alvin.TrackingTodo.Windows
         [STAThread]
         static void Main()
         {
+            //tettt
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Home());
